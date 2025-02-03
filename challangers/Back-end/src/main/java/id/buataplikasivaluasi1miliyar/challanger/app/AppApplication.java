bb2e4ca53961c4cef86c543a29382fedb9f1b5d6
abduh <muhammadabdu089@gmail.com>
2025-02-03 13:45:20 +0700

    init project back end

---
+++ challangers/Back-end/src/main/java/id/buataplikasivaluasi1miliyar/challanger/app/AppApplication.java
@@ -0,0 +1,13 @@
+package id.buataplikasivaluasi1miliyar.challanger.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
