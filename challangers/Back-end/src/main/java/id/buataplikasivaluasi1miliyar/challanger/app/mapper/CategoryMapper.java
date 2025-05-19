44d160bbf4578d3a837108857472792c031e538e
abduh <muhammadabdu089@gmail.com>
2025-05-19 21:31:42 +0700

    Update: CategoryMapper Fix

--- challangers/Back-end/src/main/java/id/buataplikasivaluasi1miliyar/challanger/app/mapper/CategoryMapper.java
+++ challangers/Back-end/src/main/java/id/buataplikasivaluasi1miliyar/challanger/app/mapper/CategoryMapper.java
@@ -6,7 +6,11 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
     CategoryDto mapToCategoryDto(Category category);
+
+    List<CategoryDto> mapToCategoryDtoList(List<Category> categories); // ← Tambahkan ini
 }
