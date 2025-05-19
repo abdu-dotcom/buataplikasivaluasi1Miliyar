806e8d1c2739c98f8a559c2905909cc664bd9f64
abduh <muhammadabdu089@gmail.com>
2025-05-19 22:18:13 +0700

    Revert "Update: CategoryMapper Fix"
    This reverts commit 44d160bbf4578d3a837108857472792c031e538e.

--- challangers/Back-end/src/main/java/id/buataplikasivaluasi1miliyar/challanger/app/mapper/CategoryMapper.java
+++ challangers/Back-end/src/main/java/id/buataplikasivaluasi1miliyar/challanger/app/mapper/CategoryMapper.java
@@ -6,11 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-import java.util.List;
-
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
     CategoryDto mapToCategoryDto(Category category);
-
-    List<CategoryDto> mapToCategoryDtoList(List<Category> categories); // ← Tambahkan ini
 }
